f0f3f7daff2276ad6a6e1d749281a8cdf9af4657
VishalGovindiya <fluttertutorial1@gmail.com>
2024-07-03 15:35:13 +0530

    Update project_route.dart

--- project_route.dart
+++ project_route.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shopio_app/screens/home/home_screen.dart';
 import 'package:shopio_app/screens/onboarding/onboarding_screen.dart';
+import 'package:shopio_app/screens/signUp/signup_screen.dart';
 import 'package:shopio_app/screens/singin/signi_screen.dart';
 import 'package:shopio_app/screens/splase/splase_screen.dart';
 
@@ -7,6 +9,8 @@ class AppRoute {
   static const splash = "/splase_screen";
   static const onboarding = "/onboarding_screen";
   static const signin = "/signin";
+  static const signup = "/signup";
+  static const home = "/home";
 
   static Route<dynamic>? onGenerateRoute(RouteSettings setting) {
     switch (setting.name) {
@@ -23,8 +27,15 @@ class AppRoute {
         return MaterialPageRoute(
           builder: (context) => const SigniScreen(),
         );
+      case signup:
+        return MaterialPageRoute(
+          builder: (context) => const SignUpScreen(),
+        );
+      case home:
+        return MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        );
     }
-    return null ;
-
+    return null;
   }
 }
