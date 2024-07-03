fe1d5ad3e3c8d269c467c1b89a4f08a47e9c2e18
VishalGovindiya <fluttertutorial1@gmail.com>
2024-07-03 16:55:54 +0530

    Update indicator.dart

--- indicator.dart
+++ indicator.dart
@@ -9,7 +9,7 @@ Widget onBoardingContainer(int index) {
           height: index == 0 ? 18 : 15,
           width: index == 0 ? 25 : 20,
           decoration: BoxDecoration(
-              color: index == 0 ? Colors.indigo : Colors.pink.shade200,
+              color: index == 0 ? Colors.indigo : Colors.grey,
               borderRadius: BorderRadius.circular(15)),
         ),
       ),
@@ -19,7 +19,7 @@ Widget onBoardingContainer(int index) {
           height: index == 1 ? 18 : 15,
           width: index == 1 ? 25 : 20,
           decoration: BoxDecoration(
-              color: index == 1 ? Colors.indigo : Colors.pink.shade200,
+              color: index == 1 ? Colors.indigo : Colors.grey,
               borderRadius: BorderRadius.circular(15)),
         ),
       ),
@@ -29,7 +29,7 @@ Widget onBoardingContainer(int index) {
           height: index == 2 ? 18 : 15,
           width: index == 2 ? 25 : 20,
           decoration: BoxDecoration(
-              color: index == 2 ? Colors.indigo: Colors.pink.shade200,
+              color: index == 2 ? Colors.indigo : Colors.grey,
               borderRadius: BorderRadius.circular(15)),
         ),
       )
